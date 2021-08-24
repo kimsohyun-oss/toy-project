a61f20b6c187fba066e80a8f7c6e71d2b377cdab
nana <mo_so@naver.com>
2021-08-24 21:51:22 +0900

    웹소켓 채팅

--- src/main/java/project/toy/controller/MessageController.java
+++ src/main/java/project/toy/controller/MessageController.java
@@ -2,7 +2,6 @@ package project.toy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
